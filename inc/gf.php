f5665f66d2f95835a2f6f1ea220af7ec2759d1ea
Nyco Agung <nyco.agung@klyp.co>
2022-01-17 08:35:10 +1000

    added false validation

--- inc/gf.php
+++ inc/gf.php
@@ -5,7 +5,7 @@ defined('ABSPATH') || die('Wordpress is not installed properly.');
 
 function klypHsGFCatchSubmission($form)
 {
-    if (empty($form['form']['id']) || ! $form['is_valid']) {
+    if (empty($form['form']['id']) || ! $form['is_valid'] || $form['is_valid'] === false) {
         return;
     }
 
